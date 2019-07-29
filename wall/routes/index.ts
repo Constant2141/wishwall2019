cb85788ee989749e0b6089df300f7ce276cdfe34
beichen <706608189@qq.com>
2019-07-29 17:35:52 +0800

    pull

--- wall/routes/index.ts
+++ wall/routes/index.ts
@@ -1,17 +1,22 @@
 const router = require('koa-router')()
 import { User } from '../utils/db/models/User'
 
+
 router.get('/create', async (ctx, next) => {
-  let newUser = await User.create({
-    username: "隔壁老王",
+  await User.create({
+    name: "隔壁老王",
     sex: "男吧"
   })
-  console.log('创建：' + JSON.stringify(newUser))
+
   ctx.body = '创建一个老王'
 })
 
-router.get('/findAll', async (ctx, next) => {
-  const allUser = await User.findAll()
+
+
+
+
+router.get('/find', async (ctx, next) => {
+  const allUser = await User.findOne()
 
   ctx.body = allUser
 })

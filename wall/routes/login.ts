066c496c0c02c9590f548e3433d4ff0023e8b843
Crazy492 <1056872781@qq.com>
2019-11-07 13:19:29 +0800

    增加树洞删除接口

--- wall/routes/login.ts
+++ wall/routes/login.ts
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const rp = require('request-promise');
 const wxConfig = require('../utils/wx/config');
 const getToken = require('../utils/jwt/getToken');
+const {sha1Ticket, getTicket} = require('../utils/wx/sdk.js')
 import { getUserInfo } from '../utils/wx/index';
 import { addUser } from '../Dao/loginDao';
 
@@ -25,12 +26,21 @@ router.get('/', async ctx => {
   ctx.body = User
 })
 
+router.get('/getConfig', async ctx => {
+  let { url } = ctx.request.query;
+   let ticket = await getTicket();
+   ctx.body= await sha1Ticket(ticket,decodeURIComponent(url));
+})
+
+
 router.get('/test', async ctx => {
   let { openid, refresh_token } = ctx.request.query
-
+  
+  
   let newAccess = await rp(`https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=${wxConfig.appid}&grant_type=refresh_token&refresh_token=${refresh_token}`)
     .then(data => {
       // console.log(data,'123',data.access_token);
+      console.log(JSON.parse(data),'=======1');
       return JSON.parse(data).access_token
     })
 
@@ -38,7 +48,7 @@ router.get('/test', async ctx => {
 
   let User = await rp(`https://api.weixin.qq.com/sns/userinfo?access_token=${newAccess}&openid=${openid}&lang=zh_CN`)
     .then(data => {
-
+      console.log(JSON.parse(data),'=======data');
       return JSON.parse(data)
     })
 
@@ -58,6 +68,10 @@ router.get('/getUserInfo', async ctx => {
 
 
   switch (userID) {
+    case '00':
+      openid = 'oO7BY6P--E2z-Q81w5lp6GpST-XM'
+      refresh_token = '27_Vks5qI5CVlSwXT8pO0pFGPQ6iWFrkNq52ziQOAgav5BGuNsyxBwuxuQIwZHVcnu-kWX5XYduD0BSc0LNZ83_0_qmpowZPlNHmKgTGScP8hE' 
+      break;
     case '01':
       openid = 'oO7BY6JRpJ26yi92rE77BEjdnSx0';//艹
       refresh_token = '26_WHNVk-RYC9_6kuMFKW4DWa1uZ-W-PFJbDujE97UGEO5cH-wOmqbE1vhGhjLX5YQRoE_3joYkI5YjSR31ApDqWe_q1s5BfFMMWjb5i-3AJJY'
--- wall/routes/treehole.ts
+++ wall/routes/treehole.ts
@@ -7,7 +7,8 @@ import { addTreeHole
        , getAllTreeHoles
        , addLikes
        , addTreeHoleComment
-       , countMyTreeHoles } from '../Dao/treeholeDao';
+       , countMyTreeHoles
+       , deleteTreeHole } from '../Dao/treeholeDao';
 
 router.post('/addTreeHole', async ctx => {
   let { text }: { text: string } = ctx.request.body
@@ -87,6 +88,24 @@ router.post('/addLikes', async ctx => {
   }
 })
 
+router.post('/deleteTreeHole', async ctx => {
+  let { treeholeId } = ctx.request.body
+    , code = 500
+    , result: Boolean;
+
+  try{
+    result = await deleteTreeHole(treeholeId);
+    code = 200
+  }catch(err) {
+    result = err.message;
+    code = 500
+  }
+
+  ctx.body = await {
+    code,
+    result
+  }
+})
 router.post('/addTreeHoleComment', async ctx => {
   let { comment, treeholeId } = ctx.request.body
     , { sex } = await parseToken(ctx)
